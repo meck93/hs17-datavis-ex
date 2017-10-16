b9e6e5f35ffac07dfb54557836baab6eff680412
meck93 <moritz.eck@gmail.com>
2017-10-16 17:31:34 +0200

    Created ex2 folder + update gitignore

---
+++ ex2/main.py
@@ -0,0 +1,16 @@
+# import pandas as pd
+# import numpy as np
+# from scipy.misc import imshow, imresize
+# import matplotlib.pyplot as plt 
+
+# from PIL import Image
+# from pylab import *
+
+class Main(object):
+	""" This is the main class of the second exercise of the 2017 DataVisualization class"""
+	
+	def print_test(self):
+		print("Test")
+		
+x = Main()
+x.print_test()	
