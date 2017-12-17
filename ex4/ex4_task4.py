28928bc333528018bf3b7caae2431eaf0ba3469c
meck93 <moritz.eck@gmail.com>
2017-12-17 20:45:04 +0100

    updated ex4_task4.jpeg: replaced corrupted datafile

--- ex4/ex4_task4.py
+++ ex4/ex4_task4.py
@@ -8,8 +8,19 @@ import ex4_reader as data_reader
 import matplotlib.pyplot as plt
 import numpy as np
 
+# z-value constants
+MIN_HEIGHT = 0.035
+MAX_HEIGHT = 19.835
+STEP_SIZE = 0.2
+
+# desired height
+HEIGHT = 1.0
+
+# x-values: list containing the heights of the plot
+heights = np.arange(MIN_HEIGHT, MAX_HEIGHT + STEP_SIZE, STEP_SIZE)
+
 # load the terrain data
-terrain_data = data_reader.read_terrain_data()
+terr_data = data_reader.read_terrain_data()
 
 # create the plot 
 fig, plot = plt.subplots()
@@ -23,28 +34,34 @@ plot.set_xlabel("longitude (X-coord)")
 plot.set_ylabel("latitude (Y-coord)")
 plot.set_aspect(1)
 
-# invert the y and x axis
+# invert the y axis
 plot.invert_yaxis()
 
-# plot the data
-terrain_plot = plot.contourf(terrain_data, alpha=1.0, cmap='terrain')
+# plot the terrain data layer
+terr_plot = plot.contourf(terr_data, cmap='terrain')
 
 # colorbar for the terrain_plot
-plot_colorbar = plt.colorbar(terrain_plot)
-plot_colorbar.set_label("elevation in meters above sea level")
+plot_colorbar = plt.colorbar(terr_plot)
+plot_colorbar.set_label("elevation [m (above sea level)]")
 
 # index for z-value at altitude: 1km
-# start at: 0.035 step size: 0.2 step 5: 1.035km
-HEIGHT = 5
+index = 0
+
+# find the correct index for the HEIGHT
+for i in range(len(heights)):
+    if heights[i] > HEIGHT:
+        index = i
+        break
 
 # load the temperature data: hour 1
-temp_hour1 = data_reader.read_geo_data('TC', 1)
+temp_hour_1 = data_reader.read_geo_data('TC', 1)
 
 # y-values: list of temperature data for location (200, 250)
-temp_data = temp_hour1[:, :, HEIGHT]
+temp_data = temp_hour_1[:, :, index]
+print(temp_data)
 
 # plotting the temperature data
-temp_plot = plot.contourf(temp_data, alpha=0.5, cmap="gist_heat_r")
+temp_plot = plot.contourf(temp_data, cmap="gist_heat_r", alpha=1.0)
 
 # colorbar for the temp_plot
 plot_colorbar = plt.colorbar(temp_plot)
